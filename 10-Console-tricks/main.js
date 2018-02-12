b74030a51bf5837cab95b34d280a17f02d339c1e
dandevri <hi@dandevri.es>
2018-02-12 20:29:03 +0100

    Add exercise 09

---
+++ 10-Console-tricks/main.js
@@ -0,0 +1,31 @@
+// Regular
+console.log('hello');
+// Interpolate
+console.log('hello I am a %s string!', '🎉')
+// Styling
+console.log('%c hello', 'font-size: 20px');
+// Warn
+console.warn('Oh noes');
+// Error
+console.error('Oh noes');
+//Info
+console.info('Info');
+// Testing for booleans
+console.assert(1 === 2, 'That is wrong');
+//clear
+console.clear();
+// DOM elements
+console.dir(p);
+// Grouping together
+dogs.forEach(dogs => {
+  console.group
+  console.log(dog.name)
+})
+// counting
+console.count('dan');
+console.count('dan');
+console.count('dan');
+// timing
+console.timing('fetching data');
+// table
+console.table
---
+++ 10-Console-tricks/notes.md
@@ -0,0 +1,7 @@
+# 14 must know dev tools
+* You have some JS on the page but you don't know where it's called; break on attribute modifications.
+* It will use a debugger (breakpoint)
+* console.error won't throw an error, only display it
+* Only use console.clear if you want to prank somebody
+* Log shows you the element
+* Dir show you the methods and properties
